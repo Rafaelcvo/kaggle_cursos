c0ff43d276da8620d09b1b6db40a11746fcc361b
Rafael Moreira <rafaelmoreiracvo@yahoo.com.br>
2022-03-14 09:39:33 -0300

    Time Plot of Hardcover Sales criado.

--- times_series/linear_regression.py
+++ times_series/linear_regression.py
@@ -2,6 +2,13 @@
 link do curso: https://www.kaggle.com/ryanholbrook/linear-regression-with-time-series
 """
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib_inline.config import InlineBackend
 
 df = pd.read_csv('datasets/book_sales.csv', index_col='Date', parse_dates=['Date']).drop('Paperback', axis=1)
 print(df.head(3))
+df['Time'] = np.arange(len(df.index))
+print(df.head(2))
+
